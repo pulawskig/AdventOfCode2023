c45aba6d7e214bb8de0b71d8c253108e407f99e2
Grzegorz Puławski <31245410+pulawskig@users.noreply.github.com>
2023-12-01 17:50:07 +0100

    Day 1

---
+++ AdventOfCode2023/Helpers/EnumerableExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventOfCode2023.Helpers;
+
+public static class EnumerableExtensions
+{
+    public static IEnumerable<T> FirstAndLast<T> (this IEnumerable<T> source)
+    {
+        yield return source.First();
+        yield return source.Last();
+    }
+}
---
+++ AdventOfCode2023/Helpers/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventOfCode2023.Helpers;
+
+public static class StringExtensions
+{
+    public static IEnumerable<string> GetLines(this string str, bool removeEmptyLines = true)
+    {
+        return str.Split(new[] { "\r\n", "\r", "\n" },
+            removeEmptyLines ? StringSplitOptions.RemoveEmptyEntries : StringSplitOptions.None);
+    }
+}
