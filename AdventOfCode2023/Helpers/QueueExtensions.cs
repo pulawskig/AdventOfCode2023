cd2fdee0803f99db05a8877741e5964e0cca9d01
Grzegorz Puławski <31245410+pulawskig@users.noreply.github.com>
2023-12-08 23:49:33 +0100

    Day 8

---
+++ AdventOfCode2023/Helpers/QueueExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace AdventOfCode2023.Helpers;
+
+public static class QueueExtensions
+{
+    public static void EnqueueRange<T>(this Queue<T> queue, IEnumerable<T> range)
+    {
+        foreach (var item in range)
+        {
+            queue.Enqueue(item);
+        }
+    }
+}
